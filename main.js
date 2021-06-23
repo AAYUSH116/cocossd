3e83f0a0238ebdc2675d9005a316056f99e5bcf0
AAYUSH116 <82816625+AAYUSH116@users.noreply.github.com>
2021-06-23 17:32:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 img="";
 status="";
-
+objects=[];
 function preload(){
     img=loadImage('dog_cat.jpg');
 }
@@ -19,17 +19,22 @@ objectDetector.detect(img,gotResult);
 }
 function draw(){
     image(img,0,0,640,520);
-    fill('#FF0000');
-    text('Dog',45,50);
-    noFill();
-    stroke('#FF0000');
-    rect(30,60,450,350);
 
-    fill(' #0000FF ');
-    text('Cat',320,120);
-    noFill();
-    stroke(' #0000FF ');
-    rect(300,90,270,320);
+    if(status!=""){
+for(i=0; i<objects.length;i++){
+document.getElementById("status").innerHTML="Status: object detected";
+
+fill('#FF0000');
+percent=floor(objects[i].confidence*100);
+text(objects[i].label+""+percent+"%",objects[i].x,objects[i].y);
+noFill();
+stroke("#FF0000");
+rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+}
+    }
+
+   
+
     
 }
 
@@ -38,4 +43,6 @@ function gotResult(error,results){
         console.log(error);
     }
     console.log(results);
+    objects=results;
+    
 }
